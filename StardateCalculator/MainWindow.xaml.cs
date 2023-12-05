84915608af7983549b26152be0e032d5bd06b92c
Phil Ekiert <philekiert@live.co.uk>
2023-12-05 10:56:54 +0000

    Add project files.

---
+++ StardateCalculator/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StardateCalculator
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int year, month, day, hour, minute;
+                int.TryParse(txtYear.Text, out year);
+                int.TryParse(txtMonth.Text, out month);
+                int.TryParse(txtDay.Text, out day);
+                int.TryParse(txtHour.Text, out hour);
+                int.TryParse(txtMinute.Text, out minute);
+
+                if (month == 0) month = 1;
+                if (day == 0) day = 1;
+
+                DateTime dt = new DateTime(year, month, day, hour, minute, 0);
+
+                Stardate stardate = new(dt);
+
+                lblTOS.Content = "TOS: " + stardate.FormatStardate(stardate.TOS);
+                lblTMP.Content = "TMP: " + stardate.FormatStardate(stardate.TMP);
+                lblFilms.Content = "Films: " + stardate.FormatStardate(stardate.Films);
+                lblTNG.Content = "TNG: " + stardate.FormatStardate(stardate.TNG);
+                lblPrimary.Content = "Primary: " + stardate.FormatStardate(stardate.Primary);
+            }
+            catch { }
+        }
+    }
+}
---
+++ StardateCalculator/StardateConverter.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Drawing;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Security.Cryptography.Xml;
+using System.Xaml;
+using System.Xml.Schema;
+
+public class Stardate
+{
+    /************************************************************
+
+    TOS    Starts  2266  0193.29  +1496.5725
+           Ends    2270  6179.58    
+    TMP    Starts  2267  5701.73  +133.07789
+           Ends    2286  8230.21
+    Films  Starts  2283  7697.87  +188.116
+           Ends    2294  9767.15
+    TNG    Starts  2323  0.0      +1000.0
+
+
+    Some key stardates for reference
+    --------------------------------
+
+    1512.0    2266    The Corbomite Maneuvre
+    5693.0    2269    Tholian Web, assume that this is 4 years on
+
+    7412.0    2279    Star Trek: The Motion Picture
+    8130.0    2285    The Wrath of Khan
+    8210.0    2285    The Search for Spock
+    8390.0    2286    The Voyage Home 
+    8454.0    2287    The Final Frontier
+    9521.0    2292    The Undiscovered country
+    9715.0    2293    Generations
+
+    ************************************************************/
+
+    private const double TOS_ROOT = 2265.1893d; // Changed from 2265.8709 for aesthetic reasons
+    private const double TOS_INCREMENT = 1496.2162d; // Changed from 1496.5725d
+    private const double TMP_ROOT = 2224.155d;
+    private const double TMP_INCREMENT = 133.07789d;
+    private const double FILMS_ROOT = 2242.08;
+    private const double FILMS_INCREMENT = 188.116;
+
+    private DateTime earthDate;
+    private double tosMetric;
+    private double tmpMetric;
+    private double filmsMetric;
+    private double tngMetric;
+
+    public string FormatStardate(object stardate)
+    {
+        if (stardate is DateTime dt)
+            return dt.ToString("dd/MM/yyyy HH:mm");
+        if (stardate is double d)
+            return ((int)(d * 100d) / 100d).ToString();
+        else
+            return "Error";
+    }
+
+    public DateTime EarthDate { get { return earthDate; } }
+    public double TOS { get { return tosMetric; } }
+    public double TMP { get { return tmpMetric; } }
+    public double Films { get { return filmsMetric; } }
+    public double TNG { get { return tngMetric; } }
+    public object Primary
+    {
+        get
+        {
+            if (tosMetric < 0)
+                return earthDate;
+            if (earthDate.Year < 2280)
+                return tosMetric < tmpMetric ? tosMetric : tmpMetric;
+            if (earthDate.Year < 2300)
+                return tmpMetric > filmsMetric ? tmpMetric : filmsMetric;
+            return filmsMetric > tngMetric ? filmsMetric : tngMetric;
+        }
+    }
+
+    public Stardate(DateTime realDate)
+    {
+
+        double secondsAlongYear = realDate.DayOfYear * 86_400d;
+        secondsAlongYear += realDate.Hour * 3_600;
+        secondsAlongYear += realDate.Minute * 60;
+        secondsAlongYear += realDate.Second;
+        secondsAlongYear += realDate.Millisecond / 1000d;
+
+        // real represents the year in terms of the whole number and a fraction.
+        double real = realDate.Year +
+                         (secondsAlongYear / (DateTime.IsLeapYear(realDate.Year) ? 31_622_400d : 31_536_000d));
+
+
+        //   T O S   M E T R I C
+
+        tosMetric = real - TOS_ROOT;
+        tosMetric *= TOS_INCREMENT;
+
+
+        //   T M P   M E T R I C
+
+        tmpMetric = real - TMP_ROOT;
+        tmpMetric *= TMP_INCREMENT;
+
+
+        //   F I L M S   M E T R I C
+
+        filmsMetric = real - FILMS_ROOT;
+        filmsMetric *= FILMS_INCREMENT;
+
+
+        //   T N G   M E T R I C
+
+        double date = (real - 2323d) * 1000d;
+        double time = ((realDate.Hour * 1440d) +
+                       (realDate.Minute * 60d) + 
+                        realDate.Second + 
+                       (realDate.Millisecond / 1000d)) / 86_400d;
+        // If the date segment is negative, we still want the time of day to appear to count up.
+        tngMetric = date < 0 ? date - time : date + time;
+
+    }
+}
